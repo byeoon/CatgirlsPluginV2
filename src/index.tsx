ce957e7a39bfa9a8132a115360ee965350bf13ea
byron <47872200+byeoon@users.noreply.github.com>
2024-03-01 19:39:49 -0500

    catboys are real now (ffs ill need to change the plugin name now)

--- src/index.tsx
+++ src/index.tsx
@@ -31,6 +31,11 @@ const nekos_life_img_types = [
    "avatar",
    "waifu"
  ];
+
+ const catboys_img_types = [
+  "img",
+  "baka"
+];
  
  async function getImageSize(file: string): Promise<any> {
    return new Promise(
@@ -101,7 +106,7 @@ const Patcher = create('CatgirlsPlugin');
                   footer: {
                     text: "nekos.life",
                   },
-                  color: "0x45f5f5",
+                  color: "0x00007d",
                 };
                 const component = {
                   type: 1,
@@ -131,8 +136,104 @@ const Patcher = create('CatgirlsPlugin');
             }
           },
         };
+
+
+
+
+        const catboys_command: Command = {
+          id: "catboys-command",
+    
+          name: "catboys",
+          displayName: "catboys",
+    
+          description: "Sends an image from the catboys.com API.",
+          displayDescription: "Sends an image from the catboys API.",
+    
+          type: ApplicationCommandType.Chat,
+          inputType: ApplicationCommandInputType.BuiltInText,
+    
+          options: [
+            {
+              name: "type",
+              displayName: "type",
+    
+              description: "Image type",
+              displayDescription: "Image type",
+    
+              type: ApplicationCommandOptionType.String,
+              required: true,
+              choices: catboys_img_types.map((x) => ({
+                name: x,
+                displayName: x,
+                value: x,
+              })),
+            },
+          ],
+    
+          execute: async function (args, message) {
+            const text = args[0].value;
+            const whisper = args[1]?.value ?? true;
+            const resp = await REST.get(`https://api.catboys.com/${text}`);
+            if (resp.ok) {
+              if (whisper) {
+                const { width, height } = await getImageSize(resp.body["url"]);
+                const embed = {
+                  type: "rich",
+                  title: text === "img" ? 'random image' : `random ${text} image`,
+                  image: {
+                    proxy_url: `https://external-content.duckduckgo.com/iu/?u=${resp.body["url"]
+                      }`,
+                    url: resp.body["url"],
+                    width: width,
+                    height: height,
+                  },
+                  footer: {
+                    text: "catboys.com",
+                  },
+                  color: "0x00007d",
+                }
+                if (text === "img" && resp.body['artist'] !== "unknown") {
+                  Object.assign(embed, {
+                    fields: [
+                      {
+                        inline: true,
+                        name: "Artist",
+                        value: `[${resp.body['artist']}](${resp.body['artist_url']})`
+                      } 
+                    ]
+                  });
+                };
+                const component = {
+                  type: 1,
+                  components: [{
+                    type: 2,
+                    style: 5,
+                    label: "View image",
+                    url: resp.body["url"],
+                  }],
+                };
+                sendReply(message?.channel.id ?? "0",
+                  {
+                    embeds: [embed],
+                    components: [component],
+                  },
+                  "catboys.com",
+                  "https://github.com/Catboys-Dev.png",
+                );
+              } else {
+                return {
+                  content: resp.body["url"],
+                };
+              }
+            } else {
+              sendReply(message?.channel.id ?? "0", `An error happened making a request to https://api.catboys.com/${text}`,
+              );
+            }
+          },
+        };
     
         this.commands.push(catgirls_command);
+        this.commands.push(catboys_command);
       },
 
    onStop() {
